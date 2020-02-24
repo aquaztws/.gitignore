eeb17fa3bf6d2e49062440ee505fad0f0db7d586
aquaztws <59536437+aquaztws@users.noreply.github.com>
2020-02-24 23:31:18 +0100

    Create SoundBoard.js

---
+++ .gitignore/SoundBoard.js
@@ -0,0 +1,44 @@
+const command = require('./commands.js')
+
+
+const Discord = require('discord.js')
+const Client  = new Discord.Client()
+
+client.login(fUisBIZ2KplSWUecFtUqGTOdYpIJ9UOj);
+
+module.exports = class soundboard  {
+  static   match(message) {
+		 return message.content.startsWith('!play') 
+	
+}
+  static  action (message) {
+
+  	/* let voiceChannel = message.guild.channels
+  	 .filter(function (channel) {return channel.type === 'voice'})
+  	 .first()
+  	 voiceChannel.join()
+  	 .then(function (connection){
+    */
+       message.member.voiceChannel.join().then(function (connection){ 
+
+  	 	let args = message.content.split(' ')
+
+		args.shift()
+		var element = args.shift();
+
+		args.shift()
+
+       if(element === "manquerderespect"){connection.playFile('./music/apprendreamanquerderespect.mp3')}
+       	       if(element === "cestlechien"){connection.playFile('./music/cestlechien.mp3')}
+                if(element === "hein"){connection.playFile('./music/hein.mp3')}
+
+
+		message.delete()
+
+  	 })
+
+		
+	}
+
+
+}
