b034decf6e07be117255136f64974d78bb6985ee
aquaztws <59536437+aquaztws@users.noreply.github.com>
2020-02-24 23:32:06 +0100

    Create tag2.js

---
+++ .gitignore/tag2.js
@@ -0,0 +1,47 @@
+const command = require('./commands.js')
+
+client.login(fUisBIZ2KplSWUecFtUqGTOdYpIJ9UOj);
+
+module.exports = class tag2 extends command {
+	static  match(message) {
+		return message.content.startsWith('!test')
+	}
+
+	static   action (message) {
+
+
+            function sleep(milliseconds) {                                    //creation de la fonction sleep
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+    
+
+
+
+
+
+
+ let args = message.content.split(' ')
+        args.shift()
+
+
+        
+
+        message.channel.send(args.join(' ')+'ok ?')
+       let user1= message.author.tag;
+       setTimeout(roulettestart, 3000);
+                                                                         //pause de 5 s le temps de repondre ok 
+
+  
+
+function roulettestart(){
+if (message.content.startsWith('ok')){                           //commence si ok est present
+
+            let user2= message.author.tag
+            message.channel.sen('yes')
+        }
+    }
